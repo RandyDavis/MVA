95dfa80648c341c3bae8a404998fdbac4a1195ee
Randy Davis <randy_davis@outlook.com>
2016-12-08 11:14:14 -0600

    Begin Unit Testing Basics
    create 02_UnitTestingBasics directory; complete 'Unit Test Basics - Pt.
    3

---
+++ testingInVS2015/02_UnitTestingBasics/MathTests/MathTests/MathTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTests
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            // arrange
+            int num1 = 2;
+            int num2 = 3;
+
+            // act
+            int sum = MyMath.Add(num1, num2);
+
+            // assert
+            Assert.AreEqual(5, sum);
+        }
+    }
+}
