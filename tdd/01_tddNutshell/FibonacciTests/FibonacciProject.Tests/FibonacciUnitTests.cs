1407bdd735582c8efa10894c6bb0368c60a28791
Randy Davis <randy_davis@outlook.com>
2016-12-14 16:38:59 -0600

    tdd initialization and first commit
    create 01_tddNutshell directory; complete TDD in a Nutshell Pt.4

---
+++ tdd/01_tddNutshell/FibonacciTests/FibonacciProject.Tests/FibonacciUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using FibonacciProject;
+
+namespace FibonacciProject.Tests
+{
+    public class FibonacciUnitTests
+    {
+        [Fact]
+        public void Fib_Given0_Return0()
+        {
+            // arrange
+            int n = 0;
+
+            // act
+            int result = Fibonacci.Fib(n);
+
+            // assert
+            Assert.Equal(0, result);
+        }
+    }
+
+}
---
+++ tdd/01_tddNutshell/FibonacciTests/FibonacciTests/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciProject
+{
+    public class Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            return 0;
+        }
+    }
+
+}
