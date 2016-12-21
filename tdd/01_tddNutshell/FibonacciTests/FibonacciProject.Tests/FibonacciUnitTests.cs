73408d4551220bd98a33505dec77a08afb2ccda9
Randy Davis <randy_davis@outlook.com>
2016-12-21 14:47:47 -0600

    Complete 'TDD in a Nutshell Pt.5'

--- tdd/01_tddNutshell/FibonacciTests/FibonacciProject.Tests/FibonacciUnitTests.cs
+++ tdd/01_tddNutshell/FibonacciTests/FibonacciProject.Tests/FibonacciUnitTests.cs
@@ -4,6 +4,14 @@ using FibonacciProject;
 
 namespace FibonacciProject.Tests
 {
+
+    // arrange
+
+
+    // act
+
+
+    // assert
     public class FibonacciUnitTests
     {
         [Fact]
@@ -17,6 +25,58 @@ namespace FibonacciProject.Tests
 
             // assert
             Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Fib_Given1_Return1()
+        {
+            // arrange
+            int n = 1;
+
+            // act
+            int result = Fibonacci.Fib(n);
+
+            // assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Given2_Return1()
+        {
+            // arrange
+            int n = 2;
+
+            // act
+            int result = Fibonacci.Fib(n);
+
+            // assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Given3_Return2()
+        {
+            // arrange
+            int n = 3;
+
+            // act
+            int result = Fibonacci.Fib(n);
+
+            // assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Fib_Given4_Return3()
+        {
+            // arrange
+            int n = 4;
+
+            // act
+            int result = Fibonacci.Fib(n);
+
+            // assert
+            Assert.Equal(3, result);
         }
     }
 
--- tdd/01_tddNutshell/FibonacciTests/FibonacciTests/Fibonacci.cs
+++ tdd/01_tddNutshell/FibonacciTests/FibonacciTests/Fibonacci.cs
@@ -10,8 +10,15 @@ namespace FibonacciProject
     {
         public static int Fib(int n)
         {
-            return 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n <= 2)
+            {
+                return 1;
+            }
+            return Fib(n - 1) + Fib(n - 2);
         }
     }
-
 }
