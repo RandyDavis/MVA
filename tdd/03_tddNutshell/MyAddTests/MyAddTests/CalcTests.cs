65050c728704870ceb53173944220c6293bd9ea1
Randy Davis <randy_davis@outlook.com>
2017-01-09 15:58:41 -0600

    complete 'TDD in a Nutshell - Unit Testing Pt. 2'; created simple Calc Unit Test

---
+++ tdd/03_tddNutshell/MyAddTests/MyAddTests/CalcTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyAddTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Add_WhenPassed1And1_Returns2()
+        {
+            // arrange
+            int x = 1;
+            int y = 1;
+
+            // act
+            int total = Calc.Add(x, y);
+
+            // assert
+            Assert.AreEqual(2, total);
+        }
+    }
+}
